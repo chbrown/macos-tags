fd86caa1be9930fb84e4ec653a06a19349d089e5
Christopher Brown <io@henrian.com>
2020-08-07 11:00:08 -0400

    Update Swift syntax

--- tags.swift
+++ tags.swift
@@ -6,25 +6,25 @@ func printHelp() {
   print("       tags write [-v] [--verbose] file [tag1 ...]")
 }
 
-func readTags(path: String) throws -> [String] {
+func readTags(_ path: String) throws -> [String] {
   let fileURL = NSURL(fileURLWithPath: path)
   var tags: AnyObject?
-  try fileURL.getResourceValue(&tags, forKey: NSURLTagNamesKey)
+  try fileURL.getResourceValue(&tags, forKey: URLResourceKey.tagNamesKey)
   return tags as? [String] ?? []
 }
 
-func writeTags(path: String, _ tags: [String]) throws {
+func writeTags(_ path: String, _ tags: [String]) throws {
   let fileURL = NSURL(fileURLWithPath: path)
-  try fileURL.setResourceValue(tags, forKey: NSURLTagNamesKey)
+  try fileURL.setResourceValue(tags, forKey: URLResourceKey.tagNamesKey)
 }
 
-enum ArgumentError: ErrorType {
+enum ArgumentError: Error {
   case HelpRequested
   case NotEnoughArguments
   case InvalidVerb(found: String)
 }
 
-func run(args: [String], verbose: Bool = false) throws {
+func run(_ args: [String], verbose: Bool = false) throws {
   guard let verb = args.first else { throw ArgumentError.NotEnoughArguments }
   let rest = args.dropFirst()
 
@@ -35,7 +35,7 @@ func run(args: [String], verbose: Bool = false) throws {
       if !tags.isEmpty || verbose {
         // I would prefer print(path, ...tags, separator: "\t"), but Swift does
         // not have an array splat operator for variadic function arguments
-        print(path, tags.joinWithSeparator("\t"), separator: "\t")
+        print(path, tags.joined(separator: "\t"), separator: "\t")
       }
     }
   } else if verb == "write" {
@@ -46,7 +46,7 @@ func run(args: [String], verbose: Bool = false) throws {
       // only print the result if the 'verbose' flag is set, but print it
       // regardless of the tags, since no tag arguments clears existing tags
       if verbose {
-        print(tags.joinWithSeparator("\t"))
+        print(tags.joined(separator: "\t"))
       }
     }
     else {
@@ -59,16 +59,16 @@ func run(args: [String], verbose: Bool = false) throws {
 
 do {
   // I wish I could do this argument processing more mutable, but it's kind of clumsy
-  var args = Process.arguments
-  // Process.arguments.first! is the path of the executed file, so we want to skip that
+  var args = CommandLine.arguments
+  // CommandLine.arguments.first! is the path of the executed file, so we want to skip that
   args.removeFirst()
   // pull off the -v --verbose flag, if provided
-  let verboseIndexOption = args.indexOf { $0 == "-v" || $0 == "--verbose" }
+  let verboseIndexOption = args.firstIndex { $0 == "-v" || $0 == "--verbose" }
   if let verboseIndex = verboseIndexOption {
-    args.removeAtIndex(verboseIndex)
+    args.remove(at: verboseIndex)
   }
   // handle the -h --help flag before handing off to run()
-  guard !args.contains({ $0 == "-h" || $0 == "--help" }) else { throw ArgumentError.HelpRequested }
+  guard !args.contains(where: { $0 == "-h" || $0 == "--help" }) else { throw ArgumentError.HelpRequested }
   try run(args, verbose: verboseIndexOption != nil)
 } catch ArgumentError.HelpRequested {
   printHelp()
